30f897f8107a5c20102f49865da7f2d51d963af4
John Naylor <jonaylor89@gmail.com>
2021-09-04 12:30:09 -0400

    middleware

--- layouts/authenticated.vue
+++ layouts/authenticated.vue
@@ -8,5 +8,6 @@
 <script>
 export default {
   layout: "authenticated",
+  middleware: "authenticated",
 };
 </script>
---
+++ middleware/authenticated.js
@@ -0,0 +1,9 @@
+
+export default function ({ route }) {
+    console.log(route.fullPath);
+
+    // If the user is not authenticated
+    //   if (!store.state.authenticated) {
+    //     return redirect('/login')
+    //   }
+}
