a4594f7ea256fcda2de0e8fe2b44a437ec75c7e7
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-09-05 22:33:39 -0400

    progress on activity.js

--- store/activity.js
+++ store/activity.js
@@ -1,4 +1,3 @@
-
 // {
 //     activities: [
 //         {
@@ -13,3 +12,40 @@
 // mutation needs a way to set ativities
 
 // actions -> pull activity and get user data from fromUserId 
+
+export const state = () => ({
+    activities: [
+    ],
+    /*
+    fromUserId: {},
+    timestamp: null,
+    type: null,
+    */
+});
+
+export const getters = {
+    
+}
+
+export const mutations = {
+    SET_ACTIVITIES(state, userData) {
+        state.fromUserId = {
+            id: userData.id,
+            email: userData.email,
+            username: userData?.username || "Anonymous",
+            bio: userData?.bio || "",
+            location: userData?.location || "",
+            loopsCount: userData?.loopsCount || 0,
+            onBoarded: userData?.onboarded || false,
+            profilePicture: userData?.profilePicture || "",
+        };
+        state.timestamp = userData.timestamp;
+        state.type = userData.type;
+    },
+};
+
+export const actions = {
+    async fetchUserActivities(userId) {
+        console.log("fetchting activities: ", usedId);
+    }
+}
